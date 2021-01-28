5c7a14eb333394f05302b21fc5baec65111fb973
karlozrilic <kazrile@gmail.com>
2021-01-28 15:20:14 +0100

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+
+mix.setPublicPath('./public')
+
+mix.copy(
+	'resources/assets/src/images',
+	'public/assets/images'
+)
+
+mix.sass(
+	'resources/assets/src/scss/main.scss',
+	'public/assets/css'
+);
+mix.js(
+	'resources/assets/src/js/app.js',
+	'public/assets/js/app.js'
+);
+
+mix.version();
+
+// browsersync watch for files included below and proxy setup
+mix.browserSync({
+	files: [
+		'resources/'
+	],
+	injectChanges: true,
+    proxy: 'http://127.0.0.1:8080',
+    port: 3000
+});
+
